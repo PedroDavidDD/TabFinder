906c3d67121070511226534ad646675a01ff9f90
PedroDavid <avariciatz@gmail.com>
2025-08-17 02:59:29 -0500

    Refactor(): Simplify tab navigation logic
    - Simplify logic for navigating between tabs in popup.js
    - Removes unnecessary code and uses modulo operator for circular navigation
    - Improves readability and maintainability by reducing complexity
    
    Modified files: popup.js
    
    Motivation: Refactoring based on code review feedback to enhance performance and clarity

--- popup.js
+++ popup.js
@@ -39,28 +39,25 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function navigateToTab(direction) {
-    chrome.tabs.query(queryOptions, async (tabs) => {
-      if (tabs.length === 0) return;
-      // Pestaña activa actual
-      const currentTab = tabs[0];
-      // Obtener todas las pestañas
-      const allTabs = await chrome.tabs.query({ currentWindow: true });
-      // Obtener la pestaña actual entre todos
-      const currentIndex = allTabs.findIndex(tab => tab.id === currentTab.id);
-      // indice [0,1,2] -> [1] +1 -> [2]
-      const newIndex = currentIndex + direction;
-      // navegacion mutable;
-      let currentTabId = 0;
+    chrome.tabs.query({ currentWindow: true }, (allTabs) => {
+      if (allTabs.length === 0) return;
 
-      if (newIndex >= 0 && newIndex < allTabs.length) {
-        currentTabId = allTabs[newIndex].id;
-      } else if (newIndex >= allTabs.length) {
-        currentTabId = allTabs[0].id;
-      } else if (newIndex < 0) {
-        currentTabId = allTabs[allTabs.length - 1].id;
-      }
+      chrome.tabs.query({ active: true, currentWindow: true }, ([currentTab]) => {
+        if (!currentTab) return;
 
-      chrome.tabs.update(currentTabId, { active: true });
+        const currentIndex = allTabs.findIndex(tab => tab.id === currentTab.id);
+        // Valida -1 significa que no se encontró la pestaña
+        if (currentIndex === -1) return;
+        // [0,1,2] -> [0]-1 = [-1] -> [0]+1 = [1] -> [1]+1 = [2] -> [2]+1 = [3]
+        let newIndex = currentIndex + direction;
+        // Ajuste circular sin condicionales: [-1 1 2 3] + 3 % 3 = [2 1 2 0];
+        // Facilita la navegacion entre pestañas por usar el operador % y el tamaño del array.
+        newIndex = (newIndex + allTabs.length) % allTabs.length;
+        // Actualiza la pestaña activa
+        chrome.tabs.update(allTabs[newIndex].id, { active: true });
+
+        loadTabs();
+      });
     });
   }
 
